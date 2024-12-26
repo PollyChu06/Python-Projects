2913394d0b139b09ababc26ea1faf4211a9f3e81
PollyChu06 <162806502+PollyChu06@users.noreply.github.com>
2024-12-26 22:19:10 +0100

    Update QRcode.py
    Added explanation to each step of the code

--- QRcode.py
+++ QRcode.py
@@ -1,9 +1,19 @@
-import qrcode
+import qrcode  # Import the qrcode library
+
+# Prompt the user to input the website link
 website = input('Type in your link: ')
 
-qr = qrcode.QRCode(version = 1, box_size = 5, border = 5)
+# Create a QRCode object with specified version, box size, and border
+qr = qrcode.QRCode(version=1, box_size=5, border=5)
+
+# Add the website data to the QRCode object
 qr.add_data(website)
+
+# Generate the QR code matrix
 qr.make()
 
-img = qr.make_image(fill_color = 'black', back_color = 'white')
+# Create a QR code image with specified fill and background colors
+img = qr.make_image(fill_color='black', back_color='white')
+
+# Save the generated QR code image to a file named 'personal_qr.png'
 img.save('personal_qr.png')
