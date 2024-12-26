c21a7736d0ae0694b52e141f61cb47c2e8481686
PollyChu06 <162806502+PollyChu06@users.noreply.github.com>
2024-12-26 22:18:14 +0100

    Update GIF.py
    Added explanation to each step of the code

--- GIF.py
+++ GIF.py
@@ -1,6 +1,15 @@
 import imageio.v3 as iio
+
+# Define a list of filenames for the images
 filenames = ['team-pic1.png', 'team-pic2.png']
-images = [ ]
+
+# Initialize an empty list to store the image data
+images = []
+
+# Loop through each filename in the list of filenames
 for filename in filenames:
+    # Read the image data from the file using imageio.imread() and append it to the images list
     images.append(iio.imread(filename))
-iio.imwrite('team.gif', images, duration = 500, loop = 0)
+
+# Write the images list to a GIF file with specified duration and loop settings
+iio.imwrite('team.gif', images, duration=500, loop=0)
